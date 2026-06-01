a5d6c952e763232cd1edfa3ab0f9480fd611e166
Gabor Nagy <mail@aigeruth.hu>
2026-06-01 13:28:00 +0100

    Enable mini-surround

--- config/plugins.nix
+++ config/plugins.nix
@@ -3,6 +3,7 @@
     fzf-lua.enable = true;
     ledger.enable = true;
     mini-comment.enable = true;
+    mini-surround.enable = true;
     trouble.enable = true;
     web-devicons.enable = true;
   };
