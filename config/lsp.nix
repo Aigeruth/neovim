f457f27479b1128f9deae88ab4648f5c6c5525ad
Gabor Nagy <mail@aigeruth.hu>
2026-07-20 11:29:32 +0100

    Enable mode LSP servers
    - Python: ruff, ty
    - CSS: TailwindCSS
    - OpenSCAD

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,11 +1,31 @@
 { lib, pkgs, ... }:
 {
   lsp.servers = {
+    # CSS
+    tailwindcss = {
+      enable = true;
+      packageFallback = true;
+    };
     html.enable = true;
+    # Nix
     nixd = {
       enable = true;
       config.settings.nixd.formatting.command = [ "${lib.getExe pkgs.nixfmt}" ];
     };
+    # OpenSCAD
+    openscad_lsp = {
+      enable = true;
+    };
+    # Python
+    ruff = {
+      enable = true;
+      packageFallback = true;
+    };
+    ty = {
+      enable = true;
+      packageFallback = true;
+    };
+    # Ruby
     rubocop = {
       enable = true;
       packageFallback = true;
